6b5bea9cb56ea6cff091363232ac75eaa5c0a8c6
alejlatorre <alejandro.latorre@pucp.edu.pe>
2023-09-19 21:15:15 -0500

    feat: added menu and rock, paper and scissors container

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+// Menu
+const timeContainer = document.getElementById('timeContainer')
+const rockPaperScissorsContainer = document.getElementById(
+    'rockPaperScissorsContainer'
+)
+
+const stopwatchBtn = document.getElementById('stopwatchBtn')
+stopwatchBtn.addEventListener('click', () => {
+    if (timeContainer.style.display == 'none') {
+        timeContainer.style.display = 'block'
+        rockPaperScissorsContainer.style.display = 'none'
+    } else {
+        timeContainer.style.display = 'none'
+    }
+})
+
+const rockPaperScissorsBtn = document.getElementById('rockPaperScissorsBtn')
+rockPaperScissorsBtn.addEventListener('click', () => {
+    if (rockPaperScissorsContainer.style.display == 'none') {
+        rockPaperScissorsContainer.style.display = 'block'
+        timeContainer.style.display = 'none'
+    } else {
+        rockPaperScissorsContainer.style.display = 'none'
+    }
+})
+
+// Stopwatch
 const timeDisplay = document.getElementById('timeDisplay')
 const startBtn = document.getElementById('startBtn')
 const pauseBtn = document.getElementById('pauseBtn')
@@ -56,4 +83,49 @@ resetBtn.addEventListener('click', () => {
     mins = 0
     secs = 0
     timeDisplay.textContent = '00:00:00'
+})
+
+// Rock, paper and scissors
+const playerText = document.getElementById('playerText')
+const computerText = document.getElementById('computerText')
+const resultText = document.getElementById('resultText')
+const choiceBtns = document.getElementsByClassName('choiceBtn')
+
+choiceBtnsArray = [...choiceBtns] // Convert to array
+
+let player
+let computer
+let result
+
+const computerOptions = {
+    1: 'ROCK',
+    2: 'PAPER',
+    3: 'SCISSORS',
+}
+
+const computerTurn = () => {
+    const randNum = Math.floor(Math.random() * 3) + 1
+    computer = computerOptions[randNum]
+}
+
+const checkWinner = () => {
+    if (player == computer) {
+        return 'Draw!'
+    } else if (computer == 'ROCK') {
+        return player == 'PAPER' ? 'You win!' : 'You lose!'
+    } else if (computer == 'PAPER') {
+        return player == 'SCISSORS' ? 'You win!' : 'You lose!'
+    } else if (computer == 'SCISSORS') {
+        return player == 'ROCK' ? 'You win!' : 'You lose!'
+    }
+}
+
+choiceBtnsArray.forEach((button) => {
+    button.addEventListener('click', () => {
+        player = button.textContent
+        computerTurn()
+        playerText.textContent = `Player: ${player}`
+        computerText.textContent = `Computer: ${computer}`
+        resultText.textContent = checkWinner()
+    })
 })
